92f3af5f7d95f64eda0cf81744c272014b70b61b
rkwitz <65245866+rkwitz@users.noreply.github.com>
2022-08-17 13:57:17 -0400

    Made it so every backend call first checks if cookie(token) is expired

--- frontend/music.js
+++ frontend/music.js
@@ -5,23 +5,28 @@
 
 async function waitVerify() {
     await new Promise(r => setTimeout(r, 240));
-    $.ajax({
-        url: "/usersPlaylists",
-        type: "GET",
-        ContentType: 'application/json',
-        headers: {"Authorization": `${login.getToken()}`},
-        success: result => {
-            let sidebar = document.getElementById("music-sidebar");
-            let playlistContainer = document.getElementById("music-content");
-            result.forEach( (e) => {
-                // create Playlist cards for each ID
-                let playlist = new PlaylistCard(e);
-                playlist.html(sidebar, playlistContainer);
-            });
-        }, error: err => {
-            alert("something went wrong with building music page");
-        }   
-    });
+    if (login.isLogged()) {
+        $.ajax({
+            url: "/usersPlaylists",
+            type: "GET",
+            ContentType: 'application/json',
+            headers: {"Authorization": `${login.getToken()}`},
+            success: result => {
+                let sidebar = document.getElementById("music-sidebar");
+                let playlistContainer = document.getElementById("music-content");
+                result.forEach( (e) => {
+                    // create Playlist cards for each ID
+                    let playlist = new PlaylistCard(e);
+                    playlist.html(sidebar, playlistContainer);
+                });
+            }, error: err => {
+                alert("something went wrong with building music page");
+            }   
+        });
+    }
+    else {
+        location.href = "/index.html";
+    }
 }
 
 $( document ).ready(function() {
